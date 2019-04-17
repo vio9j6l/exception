93f5d6c41099b3f33b1abfe5cc24a7860e91ef91
vio9j6l <jiajia.liu951ss@gmail.com>
2019-04-17 12:42:27 -0400

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,184 +14,131 @@ Revision:
 */
 public class Main {
 
-	public static void testInt() {
-		Scanner scnr = new Scanner(System.in);
-		String input;
-		int number;
+    public static void testInt() {
+        Scanner scnr = new Scanner(System.in);
+        String input;
+        int number;
 
-		try {
-			System.out.print("\nInteger");
-			System.out.print("\nEnter an integer: ");
-			input = scnr.nextLine();
-			number = Integer.parseInt(input);
-			System.out.println("This is an integer: " + number);
-		}
-		catch (NumberFormatException notAnInt) {
-			System.out.print("This is not an integer!");
-			testInt();
-		}
-		//Prompt user to continue/quit
-		//System.out.print("\nEnter any key ('q' to quit): ");
-		//quit = scnr.next().charAt(0);
-	}
+        try {
+            System.out.print("\nInteger");
+            System.out.print("\nEnter an integer: ");
+            input = scnr.nextLine();
+            number = Integer.parseInt(input);
+            System.out.println("This is an integer: " + number);
+        } catch (NumberFormatException notAnInt) {
+            System.out.print("This is not an integer!");
+            testInt();
+        }
+        //Prompt user to continue/quit
+        //System.out.print("\nEnter any key ('q' to quit): ");
+        //quit = scnr.next().charAt(0);
+    }
 
-	public static void testFloat() {
-		Scanner scnr = new Scanner(System.in);
-		String input;
-		float number;
+    public static void testFloat() {
+        Scanner scnr = new Scanner(System.in);
+        String input;
+        float number;
 
-		try {
-			System.out.println("\nFloat");
-			System.out.println("Enter a float number: ");
-			input = scnr.nextLine();
-			number = Float.parseFloat(input);
-			System.out.println("This is an Float Number: " + number);
-		}
-		catch (NumberFormatException notAnFlo) {
-			System.out.print("This is not an Float Number!");
-			testFloat();
-		}
-	}
-/*
-	public static void testString() {
-		Scanner scnr = new Scanner(System.in);
-		String input;
+        try {
+            System.out.println("\nFloat");
+            System.out.println("Enter a float number: ");
+            input = scnr.nextLine();
+            number = Float.parseFloat(input);
+            System.out.println("This is an Float Number: " + number);
+        } catch (NumberFormatException notAnFlo) {
+            System.out.print("This is not an Float Number!");
+            testFloat();
+        }
+    }
 
-		try {
-			System.out.println("\nString");
-			System.out.println("Enter a String: ");
-			input = scnr.nextLine();
-			if (scnr.hasNextInt()) {
-				throw new Exception("Entered input is integer, Please enter a String.");
+    public static void testString() {
+        Scanner scnr = new Scanner(System.in);
 
-			}
-			else if (scnr.hasNextFloat()) {
-				throw new Exception("Entered input is float, Please enter a String.");
-			}
-			else {
-				System.out.println("This is String: " + input);
-			}
-		}
+        try {
+            System.out.print("Enter a String: ");
+            if (scnr.hasNextInt()) {
+                throw new Exception("Entered input is an integer");
+            }
+            if (scnr.hasNext()) {
+                String string = scnr.nextLine();
+                if (string.length() > 1) {
+                    System.out.println("Entered input is String");
+                } else {
+                    throw new Exception("Entered input is not a String");
+                }
 
-		catch (Exception string) {
-			System.out.print(string.getMessage());
-			//System.out.print("This is not an Float Number!");
-			testString();
-		}
+            }
+        } catch (Exception notString) {
+            System.out.println(notString.getMessage());
+            testString();
+        }
+    }
 
-	}
-*/
-	public static void testString() {
-		Scanner scnr = new Scanner(System.in);
+    public static void testArray() {
+        Scanner scnr = new Scanner(System.in);
 
-		try {
-			System.out.print("Enter a String: ");
-			if (scnr.hasNextInt()) {
-				throw new Exception("Entered input is an integer");
-			}
-			if (scnr.hasNext()){
-				String string = scnr.nextLine();
-				if(string.length()>1){
-					System.out.println("Entered input is String");
-				}
-				else {
-					throw new Exception("Entered input is not a String");
-				}
+        try {
+            System.out.println("ArrayList");
+            System.out.print("Enter an ArrayList of 3 integers(Separate by a comma(,)): ");
+            String str = scnr.nextLine();
+            String[] array = str.split(",");
 
-			}
-		}
-		catch (Exception notString) {
-			System.out.println(notString.getMessage());
-			testString();
-		}
-	}
+            for (String a : array) {
+                Integer num = Integer.parseInt(a);
+                System.out.println(a);
+            }
+            if (array.length > 3) {
+                throw new ArrayIndexOutOfBoundsException("More than 3.");
+            }
+        } catch (ArrayIndexOutOfBoundsException moreThan3) {
+            System.out.println("Invalid input, be sure to not enter more than 3 integers or a alpha character. Please try again.");
+            testArray();
+        } catch (NumberFormatException notAnInt) {
+            System.out.print("Invalid input, be sure to not enter more than 3 integers or a alpha character. Please try again.");
+            testArray();
+        }
+    }/*
+    public static void testFileName() {
+        Scanner scnr = new Scanner(System.in);
+	    File file = new File(scnr.nextLine());
+	    try {
+            if {
 
-	public static void testArray() {
-			Scanner scnr = new Scanner(System.in);
-
-			try {
-				System.out.println("ArrayList");
-				System.out.print("Enter an ArrayList of 3 integers(Separate by a comma(,)): ");
-				String str = scnr.nextLine();
-				String[] array = str.split(",");
-
-				for (String a : array) {
-					Integer num = Integer.parseInt(a);
-					System.out.println(a);
-				}
-				if (array.length > 3) {
-					throw new ArrayIndexOutOfBoundsException("More than 3.");
-				}
-			}
-				catch (ArrayIndexOutOfBoundsException moreThan3) {
-					System.out.println("Invalid input, be sure to not enter more than 3 integers or a alpha character. Please try again.");
-					testArray();
-		}
-			catch (NumberFormatException notAnInt) {
-			System.out.print("Invalid input, be sure to not enter more than 3 integers or a alpha character. Please try again.");
-			testArray();
-	}
-		}
-
-	public static void testFileName() {
-		Scanner scnr = new Scanner(System.in);
-		FileWriter fileW = null;
-
-
-		try {
-			System.out.print("Enter File Name: ");
-			String name = scnr.nextLine();
-			File file = new File(name);
-			file.createNewFile();
-
-			// fix
-			throw new IOException();
-		} catch (IOException e) {
-
-			testFileName();
-		}
-	}
-
-		/*
-
-			//file = new Scanner(new File(fileName));
-			//File file = new File(fileName);
-		/*
-			try {
-				System.out.println("FileName");
-				System.out.print("Enter File Name: ");
-				fileName = scnr.nextLine();
-				fileW = new FileWriter(fileName);
-				if() {
-
-				}
-				FileInputStream file = new FileInputStream(fileName);
-				throw new FileNotFoundException("Invalid flie name, please try again.");
-			}
-			catch (FileNotFoundException f) {
-				System.out.println(f.getMessage());
-				testFileName();
-			}
-			//File file = new File(fileName);
-					//System.out.println(file.nextLine());
-			//throw new IOException();
-		}
-
-				catch (IOException e) {
-					System.out.println("Invalid flie name, please try again.");
-					testFileName();
-				}
-*/
-
-
-
-    public static void main(String[] args) {
-		//testInt();
-		//testFloat();
-		//testString();
-		//testArray();
-		testFileName();
+            }
+        }
 
     }
+*/
+
+    public static void testFileName() {
+        Scanner scnr = new Scanner(System.in);
+
+        try {
+            //System.out.print("Enter File Name: ");
+            //File f1 = new File("T:\\" + scnr.nextLine());
+            //f1.createNewFile();
+            FileWriter file = new FileWriter(scnr.nextLine());
+            file.write(System.getProperty(scnr.toString()));
+            } catch (IOException ioe) {
+            System.out.println("Try again:");
+        } catch (NullPointerException n) {
+            System.out.println("File not found. Try again:");
+        }
+    }
+
+
+
+
+
+        public static void main (String[]args){
+            //testInt();
+            //testFloat();
+            //testString();
+            //testArray();
+            testFileName();
+
+        }
+
 }
+
 
